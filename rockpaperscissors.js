077c9544b821bbae1dd9087f8cf1f143eee92aa7
Kean Jiun Ng <keanjiunng@gmail.com>
2022-09-05 20:28:49 +0800

    Added game function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -8,7 +8,7 @@ function getComputerChoice() {
 function playRound(playerSelection, computerselection) {
     playerSelection = playerSelection.toLowerCase()
     computerselection = computerselection.toLowerCase()
-
+    winnercount = 0
     if (playerSelection == "rock" && computerselection == "paper") {
         gameoutcome = "You Lose! Paper beats Rock"
     } else if (playerSelection == "paper" && computerselection == "scissors") {
@@ -17,19 +17,38 @@ function playRound(playerSelection, computerselection) {
         gameoutcome = "You Lose! Rock beats Scissors"
     } else if (playerSelection == computerselection) {
         gameoutcome = "That's a tie"
+        winnercount = 2
     } else {
         gameoutcome = "Congrats, You Win!"
+        winnercount = 1
     }
     return gameoutcome
 }
 
 // Build a function to repeat the game 5 rounds
 function game() {
+    playerwincount = 0
+    computerwincount = 0
+
     for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Please enter your selection:")
+        const computerselection = getComputerChoice()
         playRound(playerSelection,computerselection)
+        if (winnercount == 1) {
+            playerwincount += 1
+        } else if (winnercount == 2) {    
+        } else {
+            computerwincount += 1
+        }
+     }
+
+     if (playerwincount > computerwincount) {
+        console.log("Congrats, You Win!")
+     } else if (computerwincount > playerwincount) {
+        console.log("Sorry, You Lose!")
+     } else {
+        console.log("That's a tie")
      }
 }
-const playerSelection = "rock"
-const computerselection = getComputerChoice()
 
-console.log(playRound(playerSelection,computerselection))
+game()
